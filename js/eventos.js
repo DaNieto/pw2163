43b0942f7c8a561a09d9f2e9d59125b439cb3cac
DaNieto <diegonietomurillo@gmail.com>
2016-11-16 12:03:07 -0700

    Practica js

---
+++ js/eventos.js
@@ -0,0 +1,20 @@
+//DOM = Modelo de objetos del documento
+/*jquery = $ */
+var inicio = function()
+{
+	var dameClic = function()
+	{
+		$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+  	$("#txtNombre").val(data.results[0].name.first+" "+data.results[0].name.last);
+  	$("#imgFoto").attr("src",data.results[0].picture.large);
+    console.log(data.results[0].name.first+" "+data.results[0].name.last);
+   }
+}); 
+	}	
+	$("#dameClic").on("click",dameClic);
+}
+//inicializar nuestro Documento
+$(document).on("ready",inicio);
